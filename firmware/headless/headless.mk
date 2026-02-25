4e6982ee3deb1a8145336fe7c9e09195e46b509e
Kavia AI <production@kavia.ai>
2026-02-25 06:36:24 +0000

    Checkpoint 13 - cm6232b5f9

--- firmware/headless/headless.mk
+++ firmware/headless/headless.mk
@@ -3,13 +3,11 @@
 # It can be included by firmware, unit tests, and simulator makefiles.
 
 HEADLESS_SRC = \
-	$(PROJECT_DIR)/headless/can/rusefi_headless_can.c \
 	$(PROJECT_DIR)/headless/sensors/rusefi_headless_sensors.c \
 	$(PROJECT_DIR)/headless/sensors/rusefi_headless_sensors_snapshot.c \
 	$(PROJECT_DIR)/headless/esm/rusefi_headless_esm.c
 
 HEADLESS_INCDIR = \
 	$(PROJECT_DIR)/headless \
-	$(PROJECT_DIR)/headless/can \
 	$(PROJECT_DIR)/headless/sensors \
 	$(PROJECT_DIR)/headless/esm
